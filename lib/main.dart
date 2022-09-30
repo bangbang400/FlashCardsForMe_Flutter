7d84b2f996a2060241fc6626fc8a0490b491ec1b
bangbang400 <bangbang400cc@gmail.com>
2022-09-30 21:35:56 +0900

    feat:ホーム画面の作成_レイアウト位置決定

--- lib/main.dart
+++ lib/main.dart
@@ -6,19 +6,43 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterHajimeniKakuYatu',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('FlashCardsForMe'),
-        ),
-        body: Center(
-          child: Container(
-            color: Colors.grey,
-            height: 300,
-            width: 300.0,
+        title: 'FlutterHajimeniKakuYatu',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('FlashCardsForMe'),
           ),
-        )
-      ),
+          body: Container(
+            child: Center(
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.grey,
+                    height: 300.0,
+                    width: 300.0,
+                  ),
+                  Padding(padding: EdgeInsets.all(5),),
+                  Container(
+                    color: Colors.black,
+                    height: 80.0,
+                    width: 300.0,
+                  ),
+                  Padding(padding: EdgeInsets.all(5),),
+                  Container(
+                    color: Colors.black,
+                    height: 80.0,
+                    width: 300.0,
+                  ),
+                  Padding(padding: EdgeInsets.all(5),),
+                  Container(
+                    color: Colors.black,
+                    height: 80.0,
+                    width: 300.0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
     );
   }
 }
