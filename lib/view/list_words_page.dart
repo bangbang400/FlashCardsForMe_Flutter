2fad9a3b999ace7f48c496a28a41e401840ad17a
bangbang400 <bangbang400cc@gmail.com>
2022-10-12 20:25:43 +0900

    feat:DBの作成まで完了

--- lib/view/list_words_page.dart
+++ lib/view/list_words_page.dart
@@ -1,7 +1,30 @@
+import 'package:flash_cards_for_me/db/db_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flash_cards_for_me/db/words.dart';
 
-class ListWordsPage extends StatelessWidget {
+class ListWordsPage extends StatefulWidget {
   const ListWordsPage({Key? key}) : super(key: key);
+
+  @override
+  _ListWordsPageState createState() => _ListWordsPageState();
+}
+
+class _ListWordsPageState extends State<ListWordsPage> {
+
+  List<Words> wordsList = []; // all data
+  bool isLoading = false; // save table state
+
+  @override
+  void initState() {
+    super.initState();
+    getWordsList();
+  }
+
+  Future getWordsList() async {
+    setState(() => isLoading = true); // now loading
+    wordsList = await DbHelper.instance.getAllWords();
+    setState(() => isLoading = false); // loading completed
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,9 +32,29 @@ class ListWordsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('単語リスト'),
       ),
-      body: Container(
-
-      ),
+      body: isLoading ? const Center(
+        child: CircularProgressIndicator(),
+      ) : SizedBox(
+        child: ListView.builder(
+          itemCount: wordsList.length,
+          itemBuilder: (BuildContext context, int index) {
+            final word = wordsList[index];
+            return Card(
+              child: InkWell(
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    children: <Widget>[
+                      Text(word.word,
+                      style: const TextStyle(fontSize: 30),)
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      )
     );
   }
 }
