6ee3e4d828968df69be23ee133872c7a0951a6cc
bangbang400 <bangbang400cc@gmail.com>
2022-10-20 21:27:35 +0900

    feat:詳細画面:単語と説明を表示

--- lib/view/detail_word_page.dart
+++ lib/view/detail_word_page.dart
@@ -2,9 +2,9 @@ import 'package:flash_cards_for_me/db/db_helper.dart';
 import 'package:flutter/material.dart';
 import '../db/words.dart';
 
-
 class DetailWordPage extends StatefulWidget {
   final int id;
+
   const DetailWordPage({Key? key, required this.id}) : super(key: key);
 
   @override
@@ -12,14 +12,12 @@ class DetailWordPage extends StatefulWidget {
 }
 
 class _DetailWordPageState extends State<DetailWordPage> {
-
   // late :
   // lateで宣言された変数が使用されないときは初期化されない
   // 変数宣言時のthisへのアクセス
   // Null safety有効化時のUnitTestでNon-nullable
   late Words words;
   bool isLoading = false;
-
 
   @override
   void initState() {
@@ -40,14 +38,49 @@ class _DetailWordPageState extends State<DetailWordPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: isLoading ? const Center(
-          child: CircularProgressIndicator(),
-        ) : Text(words.word ?? '単語詳細'),
-      ),
-      body: Container(
+        appBar: AppBar(
+          title: isLoading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Text(words.word ?? '単語詳細'),
+        ),
+        // Row:横方向
+        // Column:縦方向
+        // mainAxisAlignment:縦方向
+        // crossAxisAlignment:横方向
+        body: Container(
+          // padding: const EdgeInsets.all(15.0),//内部に余白ができる
+          margin: EdgeInsets.all(10.0),
+          // alignment: Alignment.topCenter,
+          // alignment: const Alignment(5, 5),
+          child: Row(
+            // mainAxisAlignment:Rowの場合、横方向の中央
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  // 単語
+                  isLoading ? const Center(
+                    child: CircularProgressIndicator(),
+                  ) :
+                  Text(
+                    words.word ?? '読み込みエラー',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                  // 単語の説明
+                  isLoading ? const Center(
+                    child: CircularProgressIndicator(),
+                  ) :
+                  Text(
+                    words.commentary ?? '読み込みエラー',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ],
+              ),
 
-      ),
-    );
+            ],
+          ),
+        ));
   }
 }
