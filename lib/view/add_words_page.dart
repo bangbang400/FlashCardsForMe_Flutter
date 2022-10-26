8229f2213f91581cd4fbf9e3563fac4f32e50ecf
bangbang400 <bangbang400cc@gmail.com>
2022-10-26 21:14:21 +0900

    feat:単語登録画面:デザインを修正

--- lib/view/add_words_page.dart
+++ lib/view/add_words_page.dart
@@ -5,7 +5,9 @@ import 'package:sqflite/sqflite.dart';
 
 class AddWordsPage extends StatefulWidget {
   final Words? words;
+
   const AddWordsPage({Key? key, this.words}) : super(key: key);
+
   @override
   _AddWordsPageState createState() => _AddWordsPageState();
 }
@@ -16,6 +18,9 @@ class _AddWordsPageState extends State<AddWordsPage> {
   late String commentary;
   late DateTime createdAt;
   late DateTime modifiedAt;
+
+  double screenHeight = 0; // 画面の高さ
+  double screenWidth = 0; // 画面の横幅
 
   @override
   void initState() {
@@ -30,57 +35,80 @@ class _AddWordsPageState extends State<AddWordsPage> {
   // 単語追加ウィジェット
   @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text('単語を追加'),
       ),
       body: Container(
         child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             // 単語入力フォーム
-            Padding(padding: EdgeInsets.all(20),),
+            Padding(
+              padding: EdgeInsets.all(20),
+            ),
             Container(
-              // margin: EdgeInsets.all(20),
+              margin: EdgeInsets.all(10),
               // padding: EdgeInsets.all(20),
-              width: double.infinity,
-              color: Colors.grey,
-              child: TextField(
-                maxLength: 10,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: '10文字以内',
+              // width: double.infinity,
+              // color: Colors.yellow,
+              child: SizedBox(
+                width: double.infinity,
+                child: TextField(
+                  maxLength: 10,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30)),
+                    hintText: '単語：10文字以内',
+                  ),
+                  autofocus: true, // オートフォーカス
+                  onChanged: (word) => setState(() => this.word = word),
                 ),
-                autofocus: true, // オートフォーカス
-                onChanged: (word) => setState(() => this.word = word),
               ),
             ),
-            Padding(padding: EdgeInsets.all(5),),
+            // Padding(
+            //   padding: EdgeInsets.all(5),
+            // ),
             // 単語の解説入力フォーム
             Container(
-              // margin: EdgeInsets.all(10),
-              width: double.infinity,
-              color: Colors.grey,
-              child: TextField(
-                maxLength: 60,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: '60文字以内',
+              margin: EdgeInsets.all(10),
+              // color: Colors.yellow,
+              child: SizedBox(
+                width: double.infinity,
+                child: TextField(
+                  maxLength: 60,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30)
+                    ),
+                    hintText: '解説：60文字以内',
+                  ),
+                  autofocus: true, // オートフォーカス
+                  onChanged: (commentary) =>
+                      setState(() => this.commentary = commentary),
                 ),
-                autofocus: true, // オートフォーカス
-                onChanged: (commentary) => setState(() => this.commentary = commentary),
               ),
             ),
-            Padding(padding: EdgeInsets.all(5),),
+            // Padding(
+            //   padding: EdgeInsets.all(5),
+            // ),
             ElevatedButton(
-              onPressed: (){
+              onPressed: () {
                 // DB追加処理を実行
                 addWord();
-                },
+              },
               child: Text(
-                'DB追加',
-                style: TextStyle(fontSize: 10),
+                '単語追加',
+                style: TextStyle(fontSize: 20),
               ),
-              style:ButtonStyle(),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)
+                ),
+                fixedSize: Size(screenWidth * 0.7, 50)
+              ),
             ),
           ],
         ),
@@ -93,14 +121,14 @@ class _AddWordsPageState extends State<AddWordsPage> {
     await addWordProcess();
   }
 
-  Future addWordProcess() async{
+  Future addWordProcess() async {
     final wordData = Words(
-        word: word,
-        commentary: commentary,
-        correct: 0,
-        wrong: 0,
-        createdAt: createdAt,
-        modifiedAt: modifiedAt,
+      word: word,
+      commentary: commentary,
+      correct: 0,
+      wrong: 0,
+      createdAt: createdAt,
+      modifiedAt: modifiedAt,
     );
     await DbHelper.instance.insert(wordData);
   }
